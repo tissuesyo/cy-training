b0a6d9397b66ac58adb7adae259694395afebcee
tissuesyo <tissuesyo@gmail.com>
2019-11-22 08:00:58 +0800

    update training 2

--- cypress/integration/exercise-4/answer/exercise4.spec.ts
+++ cypress/integration/exercise-4/answer/exercise4.spec.ts
@@ -1,17 +1,32 @@
 describe('Exercise 4 Network', () => {
   it('Mock query result', () => {
     // enable response stubbing
-    cy.server();
+    // cy.server();
 
-    const json = [{ id: 1, label: 'Google', value: 'Google' }];
-    cy.fixture('brand.json').as('brand');
+    // const brandData = [{ id: 1, label: 'Google', value: 'Google' }];
+    // cy.fixture('brand.json').as('brand');
+
+    // cy.route({
+    //   method: 'GET', // Route all GET requests
+    //   url: '/brand', // that have a URL that matches '/brand'
+    //   response: '@brand' // and force the response to be: []
+    // });
+
+    // cy.visit('http://localhost:9487/query');
+
+    cy.fixture('brand.json').as('brandData');
+
+    cy.server(); // enable response stubbing
 
     cy.route({
       method: 'GET', // Route all GET requests
       url: '/brand', // that have a URL that matches '/brand'
-      response: '@brand' // and force the response to be: []
+      response: '@brandData' // and force the response to be: []
     });
 
     cy.visit('http://localhost:9487/query');
+
+    cy.route('GET', '/query/**', 'fixture:brand'); // this works
+    cy.route('GET', '/query/**', 'fx:brand'); // this also works
   });
 });
--- src/app/admin/admin-main.component.ts
+++ src/app/admin/admin-main.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AdminService } from './admin.service';
-import { ProdInfoTo } from './prod-info-to';
+import { ProdInfoTo } from './model';
 
 @Component({
   selector: 'app-admin-main',
