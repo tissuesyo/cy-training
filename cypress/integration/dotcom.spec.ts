5cf23490b867898c69657d80909a3a1a8fad2734
Tissue <tissuesyo@gmail.com>
2023-02-23 00:30:15 +0800

    update

--- cypress/integration/dotcom.spec.ts
+++ cypress/integration/dotcom.spec.ts
@@ -1,19 +1,21 @@
 context('Pizza 1 test', () => {
   before(() => {
-    cy.visit(
-      'https://www.library.ntpc.gov.tw/MainPortal/htmlcnt/doquerysinglehtmlcnt/64e667d034f34006ae5c787052995c05'
-    );
+    cy.visit('https://www.tsmc.com/english/default.htm');
   });
 
   it('Detail information form field', () => {
     const viewOption = { capture: 'viewport' };
-    cy.setResolution([1000, 1200]);
-    cy.matchImageSnapshot(viewOption);
+    // cy.setResolution([1000, 1200]);
+    // cy.matchImageSnapshot(viewOption);
+
+    cy.task('lighthouseTest').then(result => console.log(result));
+
+    cy.task('log', 'yoyoyoyoyo');
   });
 
-  it('test case 2', () => {
-    const viewOption = { capture: 'viewport' };
-    cy.setResolution([1920, 2500]);
-    cy.matchImageSnapshot(viewOption);
-  });
+  // it('test case 2', () => {
+  //   const viewOption = { capture: 'viewport' };
+  //   cy.setResolution([1920, 2500]);
+  //   cy.matchImageSnapshot(viewOption);
+  // });
 });
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -8,3 +8,54 @@ const { addMatchImageSnapshotPlugin } = require('cypress-image-snapshot/plugin')
 module.exports = (on, config) => {
   addMatchImageSnapshotPlugin(on, config);
 };
+
+const lighthouse = require('lighthouse');
+
+const reportGenerator = require('lighthouse/lighthouse-core/report/report-generator');
+
+module.exports = (on, config) => {
+  const lightConfig = require('lighthouse/lighthouse-core/config/lr-desktop-config.js');
+  on('task', {
+    lighthouseTest() {
+      console.log('Test cy task');
+      //   const report = await lighthouse(page.url(), opts, config).then(results => {
+      //     return results;
+      // });
+
+      const opts = {
+        chromeFlags: ['--headless'],
+        logLevel: 'info',
+        output: 'json',
+        disableDeviceEmulation: true,
+        defaultViewport: {
+          width: 1200,
+          height: 900
+        },
+        chromeFlags: ['--disable-mobile-emulation']
+      };
+
+      // // lighthouse('https://www.tsmc.com/english/careers/index.htm');
+      lighthouse('https://www.tsmc.com/english/careers/index.htm', opts, lightConfig).then(results => {
+        return results;
+      });
+      // const html = reportGenerator.generateReport(report.lhr, 'html');
+      // const json = reportGenerator.generateReport(report.lhr, 'json');
+
+      // fs.writeFile('report.html', html, err => {
+      //   if (err) {
+      //     console.error(err);
+      //   }
+      // });
+
+      return 'hahaha';
+    }
+  });
+
+  on('task', {
+    log(message) {
+      console.log(message);
+
+      return null;
+    }
+  });
+};
